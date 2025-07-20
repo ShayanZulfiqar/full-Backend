640fd06040d0ae6077d8a8b0b5654d4acd91a57b
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2025-07-20 18:35:01 +0500

    feat: add node-gyp-build package with initial implementation
    - Added LICENSE file with MIT License.
    - Created README.md for node-gyp-build with usage instructions and examples.
    - Introduced SECURITY.md for reporting security vulnerabilities.
    - Implemented bin.js for executing node-gyp-build commands.
    - Added build-test.js for testing prebuilds.
    - Developed index.js to handle loading of native modules.
    - Created node-gyp-build.js for managing prebuilds and native module resolution.
    - Added optional.js to handle optional native dependencies.
    - Initialized package.json with metadata, dependencies, and scripts.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         unique: true,
@@ -9,15 +10,15 @@ const userSchema = new mongoose.Schema({
         trim: true,
         index: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
         lowercase: true,
         trim: true,
-        
+
     },
-    fullName:{
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -33,19 +34,58 @@ const userSchema = new mongoose.Schema({
     },
     watchHistory: [
         {
-            type : mongoose.Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'Video'
         }
     ],
-    password:{
+    password: {
         type: String,
         required: [true, 'Password is required!']
     },
     refreshToken: {
         type: String,
-        
+
     }
 
-}, {timestamps: true})
+}, { timestamps: true })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const User = mongoose.model('User', userSchema)
--- src/models/vide.models.js
+++ src/models/vide.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 
 const videoSchema = new mongoose.Schema({
@@ -37,6 +38,8 @@ const videoSchema = new mongoose.Schema({
         ref: 'User',
     }
 }, {timestamps: true});
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 
 const Video = mongoose.model('Video', videoSchema);
